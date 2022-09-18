9d187d6ab17869edc7349e52d9f7a081f7adb1ce
jvhe <jeongbae1212@gmail.com>
2022-09-18 21:39:09 +0900

    feat: assignment #2.10 구현

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    multiply: function (a, b) {
+        console.log(a * b);
+    },
+    powerOf: function (a, b) {
+        console.log(a ** b);
+    }
+}
