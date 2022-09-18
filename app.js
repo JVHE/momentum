f5f547595d25909a53fbf836a6e4432fcdd374c7
jvhe <jeongbae1212@gmail.com>
2022-09-18 21:46:45 +0900

    modify: calculator가 값 반환하도록 수정 #2.11

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const calculator = {
     add: function (a, b) {
-        console.log(a + b);
+        return a + b;
     },
     minus: function (a, b) {
-        console.log(a - b);
+        return a - b;
     },
     divide: function (a, b) {
-        console.log(a / b);
+        return a / b;
     },
     multiply: function (a, b) {
-        console.log(a * b);
+        return a * b;
     },
     powerOf: function (a, b) {
-        console.log(a ** b);
+        return a ** b;
     }
 }
+
+const plusResult = calculator.add(10, 5);
+const minusResult = calculator.minus(plusResult, 5);
+const divideResult = calculator.divide(minusResult, 5);
+const multiplyResult = calculator.multiply(divideResult, 5);
+const powerOfResult = calculator.powerOf(multiplyResult, 5);
