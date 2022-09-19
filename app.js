2a7f01d9fe491b07c0303efcaa11d06f955d2959
jvhe <jeongbae1212@gmail.com>
2022-09-19 18:58:04 +0900

    modify: event listener 학습 #3.5

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const calculator = {
-    add: function (a, b) {
-        return a + b;
-    },
-    minus: function (a, b) {
-        return a - b;
-    },
-    divide: function (a, b) {
-        return a / b;
-    },
-    multiply: function (a, b) {
-        return a * b;
-    },
-    powerOf: function (a, b) {
-        return a ** b;
-    }
-}
+const hellos = document.querySelector('.hello');
+ 
+hellos.addEventListener('click', function () {
+    console.log(hellos);
+});
 
-const plusResult = calculator.add(10, 5);
-const minusResult = calculator.minus(plusResult, 5);
-const divideResult = calculator.divide(minusResult, 5);
-const multiplyResult = calculator.multiply(divideResult, 5);
-const powerOfResult = calculator.powerOf(multiplyResult, 5);
+window.addEventListener('resize', function () {
+    this.document.body.style.backgroundColor = 'tomato';
+});
+
+window.addEventListener('copy', function () {
+    alert('Copying is not allowed!');
+});
+
+window.addEventListener('offline', function () {
+    alert('You are offline!');
+})
+
+window.addEventListener('online', function () {
+    alert('You are online!');
+})
